5fb8a0ebc96a3a5356543e8a226774a733bd5cbb
Hillary Mason <hillary.mason@outlook.com>
2023-12-01 20:01:54 +0000

    updated PseuoCode

---
+++ assets/js/logic.js
@@ -0,0 +1,31 @@
+//start timer, change screen to show first question
+function Start() {
+
+}
+
+//event listener 
+//start()
+
+
+//Ask Question & give options
+function Question() {
+
+}
+
+
+//function to check if answer correct
+function answer() {
+
+}
+//timer will go down as questions are asked
+function timer(){
+
+}
+//save high score in local storage
+function highScore() {
+
+}
+
+
+
+
