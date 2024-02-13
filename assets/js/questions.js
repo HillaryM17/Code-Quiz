cc07435b5e6d118058d09e26490b8a52d0ccb2a6
Hillary Mason <hillary.mason@outlook.com>
2024-02-13 14:24:47 +0000

    updated questions for quiz

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,28 +1,56 @@
 var questions = [
     {
+        question:"The function and var are known as:",
+        answer: ["Keywords","Data types","Declaration statements"],
+        correct: "Declaration statements"
+    },
+    {
+        question: "Which type of JavaScript language is ___",
+        answer: ["Object-Oriented","Object-Based","Assembly-language"],
+        correct: "Object-Based"
+    },
+    {
+        question:"Which of the following variables takes precedence over the others if the names are the same?",
+        answer: ["Global variable","The local element","The two of the above","None of the above"],
+        correct: "The local element"  
+    },
+    {
+        question: "Which one of the following is the correct way for calling the JavaScript code?",
+        answer: ["Preprocessor","Triggering Event","RMI","Function/Method"],
+        correct: "Function/Method"
+    },
+    {
         
-        question: "What tag can be used to insert a line break or blank line in an HTML document?",
-        answer: ['<br></br>', "<body></body>", "<title></title>"],
-        correct: '<br></br>'
+        question: "Which of the following number object function returns the value of the number?",
+        answer: ["toString()","valueOf()","toLocaleString()"],
+        correct: "valueOf()"
     },
     {
-        question:"What tag is used to define the bottom section (footer) of an HTML document?",
-        answer: ["<button></button>", "<footer></footer>", "<title></title>"],
-        correct:"<footer></footer>"
+        question: "In JavaScript the x===y statement implies that:",
+        answer: ["Both x and y are equal in value, type and reference address as well","Both are x and y are equal in value only","Both are equal in the value and data type","Both are not same at all."],
+        correct: "Both are equal in the value and data type."
     },
     {
-        question: "What tag is used to specify a section of text that provides an example of computer code?",
-        answer: ["<caption>", "<code>", "<head></head>"],
-        correct:"<code>"
+        question: "Which one of the following is an ternary operator?",
+        answer: ["?",":","-","+"],
+        correct: "?"
     },
     {
-        question: "What tag is used to define an interactive field where users can enter data?",
-        answer: ["<datalist>", "<input>", "<dialog>"],
-        correct: "<input>"
+        
+        question: "Which one of the following is known as the Equality operator, which is used to check whether the two values are equal or not:",
+        answer: ["=","===","==","&&"],
+        correct: "=="
     },
     {
-        question: "What tag is used to render or transform text into an important (bold) version?",
-        answer: ["<strong>", "<em>", "<blockquote>"],
-        correct: "<strong>"
-    }
+        
+        question: "Which one of the following operator returns false if both values are equal?",
+        answer: ["!","!==","!="],
+        correct: "!="
+    },
+    {
+        
+        question: "In a case, where the value of the operator is NULL , the typeof returned by the unary operator is___.",
+        answer:["undefined","string","boolean","object"],
+        correct: "object"
+    },
 ]
